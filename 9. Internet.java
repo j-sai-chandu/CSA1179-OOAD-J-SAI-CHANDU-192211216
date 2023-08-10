b516cfc71906308cf18e8dd85335a4302f40af35
Jonnalagadda Sai Chandu <123460982+j-sai-chandu@users.noreply.github.com>
2023-08-10 09:24:59 +0530

    Add files via upload

---
+++ 9. Internet.java
@@ -0,0 +1,186 @@
+
+
+/**
+ * Class e_book_management_class3
+ */
+public class e_book_management_class3 {
+
+  //
+  // Fields
+  //
+
+  private void enter_id;
+  private void verify_user;
+  
+  //
+  // Constructors
+  //
+  public e_book_management_class3 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of enter_id
+   * @param newVar the new value of enter_id
+   */
+  private void setEnter_id (void newVar) {
+    enter_id = newVar;
+  }
+
+  /**
+   * Get the value of enter_id
+   * @return the value of enter_id
+   */
+  private void getEnter_id () {
+    return enter_id;
+  }
+
+  /**
+   * Set the value of verify_user
+   * @param newVar the new value of verify_user
+   */
+  private void setVerify_user (void newVar) {
+    verify_user = newVar;
+  }
+
+  /**
+   * Get the value of verify_user
+   * @return the value of verify_user
+   */
+  private void getVerify_user () {
+    return verify_user;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void check_availability()
+  {
+  }
+
+
+}
+
+
+
+/**
+ * Class Internet
+ */
+public class Internet {
+
+  //
+  // Fields
+  //
+
+  private void enter_id;
+  private void download;
+  private void login;
+  private void logout;
+  
+  //
+  // Constructors
+  //
+  public Internet () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of enter_id
+   * @param newVar the new value of enter_id
+   */
+  private void setEnter_id (void newVar) {
+    enter_id = newVar;
+  }
+
+  /**
+   * Get the value of enter_id
+   * @return the value of enter_id
+   */
+  private void getEnter_id () {
+    return enter_id;
+  }
+
+  /**
+   * Set the value of download
+   * @param newVar the new value of download
+   */
+  private void setDownload (void newVar) {
+    download = newVar;
+  }
+
+  /**
+   * Get the value of download
+   * @return the value of download
+   */
+  private void getDownload () {
+    return download;
+  }
+
+  /**
+   * Set the value of login
+   * @param newVar the new value of login
+   */
+  private void setLogin (void newVar) {
+    login = newVar;
+  }
+
+  /**
+   * Get the value of login
+   * @return the value of login
+   */
+  private void getLogin () {
+    return login;
+  }
+
+  /**
+   * Set the value of logout
+   * @param newVar the new value of logout
+   */
+  private void setLogout (void newVar) {
+    logout = newVar;
+  }
+
+  /**
+   * Get the value of logout
+   * @return the value of logout
+   */
+  private void getLogout () {
+    return logout;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void verify()
+  {
+  }
+
+
+  /**
+   */
+  public void status()
+  {
+  }
+
+
+}
---
+++ 9. user1.java
@@ -0,0 +1,145 @@
+
+
+/**
+ * Class e_book_management_class3
+ */
+public class e_book_management_class3 {
+
+  //
+  // Fields
+  //
+
+  private void enter_id;
+  private void verify_user;
+  
+  //
+  // Constructors
+  //
+  public e_book_management_class3 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of enter_id
+   * @param newVar the new value of enter_id
+   */
+  private void setEnter_id (void newVar) {
+    enter_id = newVar;
+  }
+
+  /**
+   * Get the value of enter_id
+   * @return the value of enter_id
+   */
+  private void getEnter_id () {
+    return enter_id;
+  }
+
+  /**
+   * Set the value of verify_user
+   * @param newVar the new value of verify_user
+   */
+  private void setVerify_user (void newVar) {
+    verify_user = newVar;
+  }
+
+  /**
+   * Get the value of verify_user
+   * @return the value of verify_user
+   */
+  private void getVerify_user () {
+    return verify_user;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void check_availability()
+  {
+  }
+
+
+}
+
+
+
+/**
+ * Class user1
+ */
+public class user1 {
+
+  //
+  // Fields
+  //
+
+  private void login;
+  private void logout;
+  
+  //
+  // Constructors
+  //
+  public user1 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of login
+   * @param newVar the new value of login
+   */
+  private void setLogin (void newVar) {
+    login = newVar;
+  }
+
+  /**
+   * Get the value of login
+   * @return the value of login
+   */
+  private void getLogin () {
+    return login;
+  }
+
+  /**
+   * Set the value of logout
+   * @param newVar the new value of logout
+   */
+  private void setLogout (void newVar) {
+    logout = newVar;
+  }
+
+  /**
+   * Get the value of logout
+   * @return the value of logout
+   */
+  private void getLogout () {
+    return logout;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void surf_books()
+  {
+  }
+
+
+}
