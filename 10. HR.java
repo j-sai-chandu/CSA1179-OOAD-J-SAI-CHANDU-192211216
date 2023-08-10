e6300743ca209a0318dc0820c5e79d496565b4b1
Jonnalagadda Sai Chandu <123460982+j-sai-chandu@users.noreply.github.com>
2023-08-10 12:37:13 +0530

    Add files via upload

---
+++ 10. HR.java
@@ -0,0 +1,169 @@
+
+
+/**
+ * Class candidate_1
+ */
+public class candidate_1 {
+
+  //
+  // Fields
+  //
+
+  private void name;
+  private void operation;
+  private void qualification;
+  
+  //
+  // Constructors
+  //
+  public candidate_1 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of operation
+   * @param newVar the new value of operation
+   */
+  private void setOperation (void newVar) {
+    operation = newVar;
+  }
+
+  /**
+   * Get the value of operation
+   * @return the value of operation
+   */
+  private void getOperation () {
+    return operation;
+  }
+
+  /**
+   * Set the value of qualification
+   * @param newVar the new value of qualification
+   */
+  private void setQualification (void newVar) {
+    qualification = newVar;
+  }
+
+  /**
+   * Get the value of qualification
+   * @return the value of qualification
+   */
+  private void getQualification () {
+    return qualification;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void verify()
+  {
+  }
+
+
+  /**
+   */
+  public void candidate()
+  {
+  }
+
+
+}
+
+
+
+/**
+ * Class HR
+ */
+public class HR {
+
+  //
+  // Fields
+  //
+
+  private void verification;
+  private void resume;
+  
+  //
+  // Constructors
+  //
+  public HR () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of verification
+   * @param newVar the new value of verification
+   */
+  private void setVerification (void newVar) {
+    verification = newVar;
+  }
+
+  /**
+   * Get the value of verification
+   * @return the value of verification
+   */
+  private void getVerification () {
+    return verification;
+  }
+
+  /**
+   * Set the value of resume
+   * @param newVar the new value of resume
+   */
+  private void setResume (void newVar) {
+    resume = newVar;
+  }
+
+  /**
+   * Get the value of resume
+   * @return the value of resume
+   */
+  private void getResume () {
+    return resume;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void Hr()
+  {
+  }
+
+
+}
